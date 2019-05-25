94213e0c77076d3390837baee466d05410b7495e
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-24 22:06:58 -0500

    Update Conditional3.js

--- Conditional3.js
+++ Conditional3.js
@@ -1,32 +1,33 @@
-/* Create variable bookName to store user window prompt entry. */
-var testPoint = parseInt(window.prompt("What is your total test point?"));
+/* Create variable testPoint to store user window prompt entry. */
+var testPoint = parseInt(window.prompt("Enter your test grade:"));
 
-/* Create variable testSentence to retrieve id element then add user entry to
-   the end of H3 heading. */
+/* Create variable testSentence to retrieve user id element then add user entry at the
+   end of H2 sentence heading.*/
 var testSentence = document.getElementById("score");
 testSentence.innerHTML += " " + testPoint;
 
-/* Create if and else statements to perform ranges of user test results operation; in 
-   order for the user to retrieve test score final results. */
+/* Create if and else statements to perform ranges on analytics operation
+   from user entry test score results; in order for the user to retrieve their
+   course final test score. */
 
-var a = 20;
-var b = 18;
-var c = 16;
-var d = 14;
+var a = 90;
+var b = 80;
+var c = 70;
+var d = 60;
 
 if (testPoint >= a) {
-    document.write("You will receive an A from the course.");
+    document.write("You have receive an A from the course.");
 }
     else if (testPoint >= b) {
-        document.write("You will receive an B from the course.");
+        document.write("You have receive a B from the course.");
     }
 
         else if (testPoint >= c) {
-            document.write("You will receive an C from the course.");
+            document.write("You have receive a C from the course.");
         }
 
             else if (testPoint >= d) {
-                document.write("You will receive an D from the course.");
+                document.write("You have receive a D from the course.");
             }
 else {
    document.write("You have failed this course.");
