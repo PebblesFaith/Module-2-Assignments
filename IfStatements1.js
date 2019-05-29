015b2edeae0a596f022e7ee01674341a4fa8fad4
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-29 11:03:16 -0500

    Update IfStatements1.js

---
+++ IfStatements1.js
@@ -0,0 +1,25 @@
+
+   /* Create all arithmetic operations variables in 'total#' and store the results of
+      operandOne and operandTwo for later outputs. */
+   
+   var num1 = 804;
+   var num2 = 99;
+   
+   if (num1 % 2 == 0) {
+      document.getElementById("statements1").innerHTML = "The num1 variable that represents the number " + num1 +  " is even.";
+   }
+     
+   else {
+      document.getElementById("statements1").innerHTML = "The num1 variable that represents the number " + num1 +  " is odd.";
+   }
+
+   if (num2 % 2 == 0) {
+      document.getElementById("statements2").innerHTML = "The num2 variable that represents the number " + num2 +  " is even.";
+   }
+     
+   else {
+      document.getElementById("statements2").innerHTML = "The num2 variable that represents the number " + num2 +  " is odd.";
+   }
+   
+   
+   
