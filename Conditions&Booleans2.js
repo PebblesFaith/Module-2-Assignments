5869a14298bff82c00e08b85a9e5e0dac684a3e6
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-28 22:29:43 -0500

    Update Conditions&Booleans2.js

---
+++ Conditions&Booleans2.js
@@ -0,0 +1,116 @@
+/* Try evaluating each of the following conditions using your browser console. */
+document.write("1.  ");
+document.write("3 > 4");
+document.write("<br>");
+var x = 3;
+var y = 4;
+
+if (x > y) {
+    document.write("The answer is true, three (3) is greater than four (4).");
+    console.log("The answer is true, three (3) is greater than four (4).");
+}
+
+else {
+    document.write("The answer is false, three (3) is not greater than four (4).");
+    console.log("The answer is false, three (3) is not greater than four (4).");
+}  
+document.write("<br>");
+
+document.write("<br>")
+document.write("2.  ");
+document.write("4 > 3");
+document.write("<br>");
+
+var x = 4;
+var y = 3;
+
+if (x > y) {
+    document.write("The answer is true, four (4) is greater than three (3).");
+    console.log("The answer is true, four (4) is greater than three (3).");
+}
+
+else {
+    document.write("The answer is false, four (4) is not greater than three (3).");
+    console.log("The answer is false, four (4) is not greater than three (3).");
+} 
+document.write("<br>");  
+
+document.write("<br>")
+document.write("3.  ");
+document.write("4 == 5");
+document.write("<br>");
+
+var x = 4;
+var y = 5;
+
+if (x == y) {
+    document.write("The answer is true, four (4) is equal to five (5).");
+    console.log("The answer is true, four (4) is equal to five (5).");
+}
+
+else {
+    document.write("The answer is false, four (4) is not equal to five (5).");
+    console.log("The answer is false, four (4) is not equal to five (5).");
+}   
+document.write("<br>");
+
+document.write("<br>")
+document.write("4.  ");
+document.write("4 == 4");
+document.write("<br>");
+
+var x = 4;
+var y = 4;
+
+if (x == y) {
+    document.write("The answer is true, four (4) is equal to four (4).");
+    console.log("The answer is true, four (4) is equal to four (4).");
+}
+
+else {
+    document.write("The answer is false, four (4) is not equal to four (4).");
+    console.log("The answer is false, four (4) is not equal to four (4).");
+}  
+document.write("<br>");
+
+document.write("<br>")
+document.write("5.  ");
+document.write("'John' == 'john'");
+document.write("<br>");
+
+var x = "John";
+var y = "john";
+
+if (x == y) {
+    document.write("The answer is true, John is equal to john.");
+    console.log("The answer is true, John is equal to john.");
+}
+
+else {
+    document.write("The answer is false, John is not equal to john.");
+    console.log("The answer is false, John is not equal to john.");
+}   
+document.write("<br>");
+
+document.write("<br>")
+document.write("6.  ");
+document.write("'asia' > 'asia'");
+document.write("<br>");
+
+var x = "asia";
+var y = "asia";
+
+if (x == y) {
+    document.write("The answer is true, asia is equal to asia.");
+    console.log("The answer is true, asia is equal to asia.");
+}
+
+else {
+    document.write("The answer is false, asia is not equal to asia.");
+    console.log("The answer is false, asia is not equal to asia.");
+} 
+document.write("<br>");
+document.write("Press F12 to view the console.log output.");
+
+
+
