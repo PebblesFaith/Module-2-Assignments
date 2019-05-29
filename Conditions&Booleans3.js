022e205d0d6c43e619c05d6da29ecfb65484db89
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-05-28 22:29:57 -0500

    Update Conditions&Booleans3.js

---
+++ Conditions&Booleans3.js
@@ -0,0 +1,39 @@
+/* Try it Yourself execute of the following statements (in order) in your browser console. */
+var num1 = 20;
+var num2 = 43;
+
+var isLessthan = num1 < num2;
+
+
+if (num1 < num2) {
+    document.write("The answer is true, twenty (20) is less than fourty-three (43).");
+    console.log("The answer is true, twenty (20) is less than fourty-three (43).");
+}   
+
+else {
+    document.write("The answer is true, twenty (20) is less not than fourty-three (43).");
+    console.log("The answer is true, twenty (20) is less not than fourty-three (43).");
+    
+    
+}  
+document.write("<br>");
+
+document.write("<br>");
+document.write("Variable isLessthan a boolean since its value is storing a condition, which is either true or false.")
+document.write("<br>");
+
+document.write("What type of data is 'isLessthan'? ");
+typeof isLessthan;
+
+document.write(isLessthan);
+console.log(isLessthan);
+document.write("<br>");
+
+document.write("What type of data is 'num1'? ");
+typeof num1;
+document.write(num1);
+console.log(num1);
+
+document.write("<br>");
+document.write("Press F12 to view the console.log output.");
+
