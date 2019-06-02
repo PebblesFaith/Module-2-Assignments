ce20e55f918b9e2d5c7e7a0231214f3c3bb7e079
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-02 15:59:07 -0500

    Update WhileLoops2.js

---
+++ WhileLoops2.js
@@ -0,0 +1,17 @@
+ /* Create initialize variable to check condition and if the condition is true
+    then execute code in between the curly brace and when the executed code between the 
+    curly braces is finishes go to the next variable condition. If there no more condition
+    to check the loop will stop the code execution. */
+   
+    var count = 0;
+
+    while (count < 51) {
+      document.write(count + " ");
+      count++;
+    }
+
+    
+
+  
+
+    
