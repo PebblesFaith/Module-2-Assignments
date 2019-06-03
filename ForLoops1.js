70747405d8d356447a213046fd0f3b59984ca40f
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-06-03 16:05:49 -0500

    Update ForLoops1.js

---
+++ ForLoops1.js
@@ -0,0 +1,13 @@
+
+
+/* Create a 'for' loop to ensure the loop will run specific number of times between 1
+   to 50 because each loop iteration contains three (3) javascript statements:
+   1) initialization,
+   2) condition(s), and
+   3) change, modification or steps.  */
+   
+for (count = 1; count < 51; count++) {  
+
+    document.write("<font color = 'white'>" + count + ' ' +  "<font>");
+}
+
